3e948ccabe8e5ec5b817a966a4eb3aac4967543f
eldusastig <144524582+eldusastig@users.noreply.github.com>
2024-05-15 02:40:45 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1ni3MRQ8PkGQJwnZJk9kMeFDyyaGaF6wJ
 """
 import tensorflow as tf
+import streamlit as st
 
 @st.cache_resource
 def load_model():
