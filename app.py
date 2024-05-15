b90cb9629750e078fac1c2d166e7f4edc3ec822d
eldusastig <144524582+eldusastig@users.noreply.github.com>
2024-05-15 17:54:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,18 +12,6 @@ st.write("""
 file=st.file_uploader("Choose an bean leaf photo",type=["jpg","png"])
 
 import cv2
-from PIL import Image,ImageOps
-import numpy as np
-
-# def import_and_predict(image_data,model):
-#     size=(128,128)
-#     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-#     img=np.asarray(image)
-#     img_reshape=img[np.newaxis,...]
-#     prediction=model.predict(img_reshape)
-#     return prediction
-
-import cv2
 from PIL import Image, ImageOps
 import numpy as np
 
